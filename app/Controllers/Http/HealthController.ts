d0c7151b95b9982bf008af96bfd64b7e035ffa81
Sairan Serra <sairan.serra@gmail.com>
2023-07-31 09:07:51 -0300

    feature - documentation

--- app/Controllers/Http/HealthController.ts
+++ app/Controllers/Http/HealthController.ts
@@ -2,8 +2,10 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class HealthController {
   public async health({ response }: HttpContextContract) {
-    return response
-      .status(200)
-      .json({ developedBy: 'Sairan Serra', statusCode: 200, documentation: 'documentation' })
+    return response.status(200).json({
+      developedBy: 'Sairan Serra',
+      statusCode: 200,
+      documentation: 'https://documenter.getpostman.com/view/21994723/2s9Xxtyvug',
+    })
   }
 }
